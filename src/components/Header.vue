7a42f4268e9c8d6939e19ba7692824497412ae67
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-05-07 15:16:21 -0300

    utilizacao do header através do props acertos

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-navbar :shadow="true">
+      <template slot="brand">
+        <b-navbar-item :tag="home" :to="{ path: '/' }">
+          <b>Quiz</b>
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item href="#">Acertos: {{acertos}}</b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'acertos'
+  ]
+}
+</script>
