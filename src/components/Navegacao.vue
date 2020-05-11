c69d2540a8fddbbd0be9cc0c38da5adac87d3d17
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-05-11 15:58:23 -0300

    criacao de um componente para navegacao

---
+++ src/components/Navegacao.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="has-text-centered mt">
+    <b-button @click="perguntaAnterior">Anterior</b-button>
+    <b-button @click="salvarPergunta">Salvar</b-button>
+    <b-button @click="proximaPergunta">Próximo</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    perguntaAnterior() {
+      this.$emit('alterarPergunta', -1);
+    },
+    proximaPergunta() {
+      this.$emit('alterarPergunta', 1);
+    },
+    salvarPergunta() {
+      this.$emit('salvarPergunta');
+    },
+  },
+};
+</script>
+
+<style  scoped>
+.mt {
+  margin-top: 10px;
+}
+</style>
