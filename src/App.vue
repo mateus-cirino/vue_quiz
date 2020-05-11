18611936c6638f52a64c4c514928fd295bf18728
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-05-11 15:59:36 -0300

    divisao em componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <Header :acertos="acertos" />
+    <Header :quantidadeDePerguntasRespondidas="quantidadeDePerguntasRespondidas"
+    :quantidadeDePerguntas="quantidadeDePerguntas"/>
     <QuestionBox
-    :pergunta="perguntaAtual"
-    @atualizarNumAcerto="atualizarNumAcerto"
-    v-if="haPerguntaAtual" />
+      :pergunta="perguntaAtual"
+      @enviarResposta="enviarResposta"
+      v-if="haPerguntaAtual"
+    />
+    <Navegacao
+    @alterarPergunta="alterarPergunta"
+    @salvarPergunta="salvarPergunta" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import QuestionBox from './components/QuestionBox.vue';
+import Navegacao from './components/Navegacao.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     QuestionBox,
+    Navegacao,
   },
   data() {
     return {
       perguntas: [],
+      respotaTemporaria: '',
       index: 0,
       acertos: 0,
+      quantidadeDePerguntas: 0,
     };
   },
   mounted() {
@@ -31,15 +40,26 @@ export default {
     })
       .then((response) => response.json())
       .then((responseJson) => {
-        this.perguntas = responseJson.results;
+        this.perguntas = [...responseJson.results];
+        this.perguntas.map((p) => [...p, { resposta: '' }]);
+        this.quantidadeDePerguntas = this.perguntas.length;
       });
   },
   methods: {
-    atualizarNumAcerto(acerto) {
-      if (acerto) {
-        this.acertos += 1;
+    enviarResposta(resposta) {
+      this.respotaTemporaria = resposta;
+    },
+    salvarPergunta() {
+      this.perguntas[this.index].resposta = this.respotaTemporaria;
+    },
+    alterarPergunta(direcao) {
+      if (direcao > 0) {
+        if (this.haProximaPergunta) {
+          this.index += 1;
+        }
+      } else if (this.haPerguntaAnterior) {
+        this.index -= 1;
       }
-      this.index += 1;
     },
   },
   computed: {
@@ -48,6 +68,15 @@ export default {
     },
     haPerguntaAtual() {
       return !!this.perguntas[this.index];
+    },
+    haProximaPergunta() {
+      return this.index < this.perguntas.length - 1;
+    },
+    haPerguntaAnterior() {
+      return this.index > 0;
+    },
+    quantidadeDePerguntasRespondidas() {
+      return this.perguntas.filter((p) => p.resposta !== '').length;
     },
   },
 };
