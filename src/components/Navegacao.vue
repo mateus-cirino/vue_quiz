bed8985971797653f78f790bff8a6a54ed7b1c07
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-05-13 21:57:56 -0300

    melhorias no componente navegacao

--- src/components/Navegacao.vue
+++ src/components/Navegacao.vue
@@ -3,11 +3,16 @@
     <b-button @click="perguntaAnterior">Anterior</b-button>
     <b-button @click="salvarPergunta">Salvar</b-button>
     <b-button @click="proximaPergunta">Próximo</b-button>
+    <b-button @click="atualizarResultado" class="button is-info">{{
+      estadoDoBotao
+    }}</b-button>
+    <b-button @click="enviarResposta" class="button is-success">Enviar</b-button>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['mostrarResultado'],
   methods: {
     perguntaAnterior() {
       this.$emit('alterarPergunta', -1);
@@ -18,11 +23,25 @@ export default {
     salvarPergunta() {
       this.$emit('salvarPergunta');
     },
+    atualizarResultado() {
+      this.$emit('atualizarResultado');
+    },
+    enviarResposta() {
+      this.$emit('enviarResposta');
+    },
+  },
+  computed: {
+    estadoDoBotao() {
+      if (!this.mostrarResultado) {
+        return 'Status das perguntas';
+      }
+      return 'Voltar para as perguntas';
+    },
   },
 };
 </script>
 
-<style  scoped>
+<style scoped>
 .mt {
   margin-top: 10px;
 }
