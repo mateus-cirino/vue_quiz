1e28179fa39ccf3665d40dc92c9655ee8b3c6e88
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-05-13 21:58:13 -0300

    melhorias no componente pai

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,19 @@
   <div id="app">
     <Header :quantidadeDePerguntasRespondidas="quantidadeDePerguntasRespondidas"
     :quantidadeDePerguntas="quantidadeDePerguntas"/>
+    <Resultados
+    :perguntas="perguntas"
+    @voltarParaAPergunta="voltarParaAPergunta"
+    v-if="mostrarResultado" />
     <QuestionBox
       :pergunta="perguntaAtual"
       @enviarResposta="enviarResposta"
-      v-if="haPerguntaAtual"
+      v-if="!mostrarResultado"
     />
     <Navegacao
+    :mostrarResultado="mostrarResultado"
+    @enviarResposta="finalizar"
+    @atualizarResultado="atualizarResultado"
     @alterarPergunta="alterarPergunta"
     @salvarPergunta="salvarPergunta" />
   </div>
@@ -17,6 +24,7 @@
 import Header from './components/Header.vue';
 import QuestionBox from './components/QuestionBox.vue';
 import Navegacao from './components/Navegacao.vue';
+import Resultados from './components/Resultados.vue';
 
 export default {
   name: 'App',
@@ -24,6 +32,7 @@ export default {
     Header,
     QuestionBox,
     Navegacao,
+    Resultados,
   },
   data() {
     return {
@@ -32,6 +41,7 @@ export default {
       index: 0,
       acertos: 0,
       quantidadeDePerguntas: 0,
+      mostrarResultado: false,
     };
   },
   mounted() {
@@ -60,13 +70,33 @@ export default {
         this.index -= 1;
       }
     },
+    atualizarResultado() {
+      this.mostrarResultado = !this.mostrarResultado;
+    },
+    voltarParaAPergunta(numeroDaPergunta) {
+      this.index = numeroDaPergunta;
+      this.atualizarResultado();
+    },
+    finalizar() {
+      const pontuacao = this.perguntas.reduce((inicio, pergunta) => {
+        if (pergunta.resposta !== '' && pergunta.correct_answer === pergunta.resposta) {
+          return inicio + 1;
+        }
+        return inicio;
+      }, 0);
+      this.alert(pontuacao);
+    },
+    alert(pontuacao) {
+      this.$buefy.dialog.alert({
+        title: 'Resultado final',
+        message: `Você acertou ${pontuacao} questões`,
+        type: 'is-success',
+      });
+    },
   },
   computed: {
     perguntaAtual() {
       return this.perguntas[this.index] || {};
-    },
-    haPerguntaAtual() {
-      return !!this.perguntas[this.index];
     },
     haProximaPergunta() {
       return this.index < this.perguntas.length - 1;
